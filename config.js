6d55d42e68aa389fab313d4db986757c96b8f288
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-03-03 14:44:53 -0700

    Setup the config file so that I can test locally

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 //var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
-var conString = "postgres://jelniepednuqjd:l7Lso4hxZqpqGYHQHBC1hVl75_@ec2-54-83-3-38.compute-1.amazonaws.com:5432/ddoccuqthfn0l1";
+// var conString = "postgres://jelniepednuqjd:l7Lso4hxZqpqGYHQHBC1hVl75_@ec2-54-83-3-38.compute-1.amazonaws.com:5432/ddoccuqthfn0l1";
+// var conString = process.env.DATABASE_URL || require('./.env');
+var conString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
 
 module.exports = conString;
