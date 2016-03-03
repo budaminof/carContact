dedc6664175fa42a27dc107dce290b9f9d227246
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-03-02 19:50:35 -0700

    More style updates

--- client/public/javascripts/script.js
+++ client/public/javascripts/script.js
@@ -11,27 +11,33 @@ $(function(){
 
   $('.leftColumnImage').on('click', function(){
     $('.plateMessageGroup').show();
+    $('.mainBodyFadeOut').fadeOut(500);
   })
 
   $('.centerColumnImage').on('click', function(){
     $('.registrationForm').show();
+    $('.mainBodyFadeOut').fadeOut(500);
     //$( '.registrationForm' ).dialog({ modal: true });
   });
 
   $('.rightColumnImage').on('click', function(){
     $('.FAQ').show();
+    $('.mainBodyFadeOut').fadeOut(500);
   });
 
   $('.cancelButtonRegister').on('click', function(){
     $('.registrationForm').hide();
+    $('.mainBodyFadeOut').fadeIn(500);
   });
 
   $('.cancelButtonMessages').on('click', function(){
     $('.plateMessageGroup').hide();
+    $('.mainBodyFadeOut').fadeIn(500);
   });
 
   $('.cancelButtonFAQ').on('click', function(){
     $('.FAQ').hide();
+    $('.mainBodyFadeOut').fadeIn(500);
   });
 
 });
