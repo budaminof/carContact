0114542c5828bb935cb0dfcfdd53256546d9b3d0
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-03-02 17:03:25 -0700

    Completely redid styling

--- client/public/javascripts/script.js
+++ client/public/javascripts/script.js
@@ -5,10 +5,33 @@ $(function(){
 
   $('.messages').hide();
 
-  $('.showRegisterForm').on('click', function(){
+  $('.plateMessageGroup').hide();
+
+  $('.FAQ').hide();
+
+  $('.leftColumnImage').on('click', function(){
+    $('.plateMessageGroup').show();
+  })
+
+  $('.centerColumnImage').on('click', function(){
     $('.registrationForm').show();
-    $('.showRegisterForm').hide();
+    //$( '.registrationForm' ).dialog({ modal: true });
+  });
+
+  $('.rightColumnImage').on('click', function(){
+    $('.FAQ').show();
+  });
+
+  $('.cancelButtonRegister').on('click', function(){
+    $('.registrationForm').hide();
+  });
+
+  $('.cancelButtonMessages').on('click', function(){
     $('.plateMessageGroup').hide();
+  });
+
+  $('.cancelButtonFAQ').on('click', function(){
+    $('.FAQ').hide();
   });
 
 });
